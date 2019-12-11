543613982743a95fa648d570814751ddd570af38
Eiren Rain <eirenliel@users.noreply.github.com>
2019-12-11 19:38:53 +0300

    Private API only uses POST

--- src/index.js
+++ src/index.js
@@ -16,23 +16,20 @@ publicApi.use(errorHandler);
 
 const privateApi = express();
 privateApi.use(bodyParser.json({limit: '1mb'}));
-privateApi.all('/set', (req, res, next) => {
-    if(req.query.handle) {
-        assert.ok(typeof(req.query.handle) === 'string', 'Handle must be a string');
-        req.body.handle = decodeURIComponent(req.query.handle);
+privateApi.post('/set', (req, res, next) => {
+    if(req.body.handle) {
+        assert.ok(typeof(req.body.handle) === 'string', 'Handle must be a string');
     }
-    if(req.query.target) {
-        assert.ok(typeof(req.query.target) === 'string', 'Target must be a string');
-        req.body.target = decodeURIComponent(req.query.target);
+    if(req.body.target) {
+        assert.ok(typeof(req.body.target) === 'string', 'Target must be a string');
     }
     if(config.privatePassword) {
         assert.ok(typeof(req.query.pwd) === 'string', 'Password must be a string');
-        req.body.pwd = decodeURIComponent(req.query.pwd);
-        assert.ok(req.query.pwd === config.privatePassword, 'Private API password does not match');
+        assert.ok(req.body.pwd === config.privatePassword, 'Private API password does not match');
     }
     next();
 });
-privateApi.all('/set', require('./privateHandlers.js').set);
+privateApi.post('/set', require('./privateHandlers.js').set);
 privateApi.use(errorHandler);
 
 mongoose.connect(config.mongoDbUrl, config.mongooseOptions).then(() => {
