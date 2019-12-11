adafd118afaefc5d1e31794f7642a72f0834869d
Eiren Rain <eirenliel@users.noreply.github.com>
2019-12-11 19:25:35 +0300

    Fix private api filters

--- src/index.js
+++ src/index.js
@@ -16,11 +16,11 @@ publicApi.use(errorHandler);
 
 const privateApi = express();
 privateApi.use(bodyParser.json({limit: '1mb'}));
-privateApi.get('/set', (req, res, next) => {
+privateApi.all('/set', (req, res, next) => {
     if(req.query.handle) {
         assert.ok(typeof(req.query.handle) === 'string', 'Handle must be a string');
         req.body.handle = decodeURIComponent(req.query.handle);
-    }  
+    }
     if(req.query.target) {
         assert.ok(typeof(req.query.target) === 'string', 'Target must be a string');
         req.body.target = decodeURIComponent(req.query.target);
